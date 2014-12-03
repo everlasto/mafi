ff03ea66fb812bbe8b61cb3ef1b3d0db6740edf7
prashanth.a <prashanth.a@flipkart.com>
2014-12-03 20:19:52 +0530

    Added slim initialization -r:self

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-echo "Welcome";
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+use Slim\Slim;
+
+//Initialize app
+$app = new Slim(array(
+    'debug' => true
+));
+
+//Specify your application name
+$app->setName('mafia-game');
+
+//Gets the documentation for usage
+$app->get( '/', function(){
+	echo "Welcome to mafia!";
+});
+
+$app->run();
 
 ?>
